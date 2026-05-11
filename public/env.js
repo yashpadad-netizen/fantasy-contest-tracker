daefe33a510ce5bd8ba65de3638fc41c68d83ad9
Yash Padad <yash.padad@pif.org.in>
2026-05-11 14:54:53 +0530

    change

--- public/env.js
+++ public/env.js
@@ -1,2 +1,4 @@
 window.__env = window.__env || {};
-window.__env.API_BASE = 'http://localhost:4000/api';
+window.__env.API_BASE = window.__env.API_BASE || (window.location.hostname === 'localhost'
+  ? 'http://localhost:4000/api'
+  : '/api');
